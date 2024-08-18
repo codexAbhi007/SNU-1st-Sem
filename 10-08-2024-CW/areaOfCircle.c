83ee1fc6d83091b4cc1e62c9de27c84043db645f
codexAbhi007 <codewithabhi2024@gmail.com>
2024-08-18 15:29:19 +0530

    change

--- 10-08-2024-CW/areaOfCircle.c
+++ 10-08-2024-CW/areaOfCircle.c
@@ -5,7 +5,7 @@
 
 int main(){
 	float r;
-	printf("Enter radius of circle:\n");
+	printf("Enter radius of the circle:\n");
 	scanf("%f",&r);
 	printf("Area of circle is %.2f sq units", PI*r*r);
 	return 0;
